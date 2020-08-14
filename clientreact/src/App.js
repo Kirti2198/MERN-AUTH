83298b28b7a692de7ca07cfdfaf59773ca81d9f1
Kirti2198 <kirti2198@gmail.com>
2020-08-14 21:59:07 +0530

    changed some routes

--- clientreact/src/App.js
+++ clientreact/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 
-function App(){
-  return (
-    <div>
-      App
-    </div>
-  );
+
+class App extends React.Component {
+   render() {
+      return (
+         <h1>App</h1>
+      );
+   }
 }
+
 export default App;
